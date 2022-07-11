e94881a4098ad797a3abd774bf02090df3212d30
Francis Ong <francis.ong25@gmail.com>
2022-07-11 23:58:26 +0800

    add .env example

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 import express from "express";
-
+import dotenv from "dotenv";
 import postRoutes from "./routes/post.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
@@ -13,9 +14,8 @@ app.use(cors());
 
 app.use('/post', postRoutes);
 
-const CONNECTION_URL = "mongodb+srv://memoriesapp:memoriesapp123@cluster0.fbgxb.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL).then(() => app.listen(PORT, () => console.log(`Connection running at port ${PORT}`)))
+mongoose.connect(process.env.CONNECTION_URL).then(() => app.listen(PORT, () => console.log(`Connection running at port ${PORT}`)))
   .catch((error) => console.log(error.message));
 
