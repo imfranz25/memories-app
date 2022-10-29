f9b60e7159a79e3a927363f94696d48c8fcddaef
Francis Ong <francis.ong25@gmail.com>
2022-10-29 14:15:25 +0800

    Clear files, remake memories app

--- client/src/api/index.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const URL = "https://francis-memories.herokuapp.com/post";
-
-export const fetchPost = () => axios.get(URL);
-export const createPost = (newPost) => axios.post(URL, newPost);
-export const updatePost = (id, updatedPost) => axios.patch(`${URL}/${id}`, updatedPost);
-export const deletePost = (id) => axios.delete(`${URL}/${id}`);
-export const likePost = (id) => axios.patch(`${URL}/${id}/likePost`);
