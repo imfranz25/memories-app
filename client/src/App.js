bb09f966a692a016b7ca4528ab8c944c3af94409
Francis Ong <francis.ong25@gmail.com>
2022-10-30 20:37:28 +0800

    styles: add styles for header

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import { Container, AppBar, Typography, Grid, Grow } from '@mui/material';
+
+// Resources
+import './app.css';
+import memories from './images/memories.png';
+
+// Components
+import Form from './components/Form/Form';
+import Posts from './components/Posts/Posts';
 
 function App(props) {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar className="app-bar" position="static">
+        <Typography className="heading" variant="h2" align="center">
+          Memories
+          <img className="image" src={memories} alt="memories" />
+        </Typography>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} md={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
