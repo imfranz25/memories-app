54bc3dfe700c930428c14208250fc712654af633
Francis Ong <francis.ong25@gmail.com>
2022-10-30 12:27:55 +0800

    Add postmessage model

---
+++ server/models/postMessage.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  creator: {
+    type: String,
+    required: true,
+  },
+  tags: [String],
+  selectedFile: String,
+  likeCount: {
+    type: Number,
+    default: 0,
+  },
+  createAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+const postMessage = mongoose.model('post', postSchema);
+
+export default postMessage;
