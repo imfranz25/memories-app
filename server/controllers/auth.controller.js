cfc6a519b7e1deb2667a886403a2e7ae3a958d0c
Francis Ong <francis.ong25@gmail.com>
2022-11-06 22:52:26 +0800

    add bcrypt for password hash

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,17 +1,29 @@
+import bcrypt from 'bcrypt';
+
 import User from '../models/user';
 
 const createUser = async (req, res, next) => {
-  const { email } = req.body;
+  const { email, password, confirmPassword } = req.body;
+
+  if (password !== confirmPassword) {
+    return res.status(422).json({ message: 'Password and Confirm Password does not match!' });
+  }
 
   try {
-    const isUserExist = User.findOne({ email: email });
+    const isUserExist = await User.findOne({ email: email });
 
     if (isUserExist) {
       return res.status(422).json({ message: 'Email already taken, please try again...' });
     }
 
-    const newUser = new User(req.body);
-    const createdUser = newUser.save();
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    if (hashedPassword) {
+      return res.status(409).json({ message: 'Something went wrong, please try again...' });
+    }
+
+    const newUser = new User({ ...req.body, password: hashedPassword });
+    const createdUser = await newUser.save();
 
     res.status(201).json(createdUser);
   } catch (error) {
