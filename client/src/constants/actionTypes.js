48694c58e7778bd072187fb9383fe42aece58f27
Francis Ong <francis.ong25@gmail.com>
2022-11-12 18:59:25 +0800

    add auth and logout constants

--- client/src/constants/actionTypes.js
+++ client/src/constants/actionTypes.js
@@ -1,4 +1,9 @@
+// Post Type Constants
 export const FETCH_ALL = 'FETCH_ALL';
 export const CREATE = 'CREATE';
 export const DELETE = 'DELETE';
 export const UPDATE = 'UPDATE';
+
+// Authentication Type Constants
+export const AUTH = 'AUTH';
+export const LOGOUT = 'LOGOUT';
