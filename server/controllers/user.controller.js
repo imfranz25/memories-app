5743838e5736f3eaf5be0caed62001650c1970e6
Francis Ong <francis.ong25@gmail.com>
2022-11-13 20:53:14 +0800

    add jwt token

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,11 +1,12 @@
 // 3rd Party Modules
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import { validationResult } from 'express-validator';
 
 // Models
 import User from '../models/user.model.js';
 
-const signUp = async (req, res, next) => {
+const signUp = async (req, res) => {
   const errors = validationResult(req);
   const { password } = req.body;
 
@@ -25,20 +26,45 @@ const signUp = async (req, res, next) => {
 
     res.status(201).json(createdUser);
   } catch (error) {
-    console.log(error);
-    next(error);
+    res.send(500).json({ error: error });
   }
 };
 
-const signIn = async (req, res, next) => {
+const signIn = async (req, res) => {
   const errors = validationResult(req);
   const { email, password } = req.body;
+  const JWT_SECRET = process.env.JWT_SECRET;
+
+  if (!errors.isEmpty) {
+    return res.status(422).json({ message: 'Invalid Input', errors: errors.array() });
+  }
 
   try {
+    const existingUser = await User.findOne({ email: email });
+
+    if (!existingUser) {
+      return res.status(404).json({ message: 'Invalid email or password...' });
+    }
+
+    const isPasswordMatched = await bcrypt.compare(password, existingUser?.password);
+
+    if (!isPasswordMatched) {
+      return res.status(401).json({ message: 'Invalid email or password...' });
+    }
+
+    const token = jwt.sign(
+      {
+        userId: existingUser._id.toString(),
+        email: existingUser.email,
+      },
+      JWT_SECRET,
+      { expiresIn: '1h' }
+    );
+
+    res.status(200).json({ message: 'Logged in success', token: token });
   } catch (error) {
-    console.log(error);
-    next(error);
+    res.send(500).json({ error: error });
   }
 };
 
-export { signUp };
+export { signUp, signIn };
